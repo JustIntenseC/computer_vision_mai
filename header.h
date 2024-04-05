f26f3143271f9abbc684be97f2ae116a1e28c6bf
Telenkov <109632817+Logar1t@users.noreply.github.com>
2024-04-05 20:08:21 +0300

    Update

---
+++ header.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <httplib.h>
+#include "nlohmann/json.hpp"
+
+using namespace httplib;
+using namespace std;
+using namespace cv;
+
+using json = nlohmann::json;
